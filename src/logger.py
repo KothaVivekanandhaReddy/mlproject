b9456b25bc068b8f7d74394bd82b652c07231033
KothaVivekanandhaReddy <vivekreddy052005@gmail.com>
2026-06-03 15:25:16 +0530

    Logging and exception

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"logs_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d %(name)s %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    logger.info("Logging setup is complete.")
